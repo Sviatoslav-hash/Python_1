b6225bdb186602b321c9deb09cb5caa38858f89f
Swiatoslaw <59115464+Sviatoslav-hash@users.noreply.github.com>
2023-04-21 21:03:00 +0200

    My first Python App

---
+++ main.py
@@ -0,0 +1,8 @@
+# print("Here I am")
+#THis is some sort of a comment, that adds some meaning
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)
+# input("Press enter to exit.")
